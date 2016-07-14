e566dc82ff961bc76189db60e0393daa064ad4a6
Andrew Katz <andrewkz123@yahoo.com>
2016-07-14 12:04:39 -0400

    more exercises

---
+++ ex40.py
@@ -0,0 +1,18 @@
+aditi = {
+  'name': 'Aditi',
+  'email': 'aditi@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+  print aditi.email
+}
